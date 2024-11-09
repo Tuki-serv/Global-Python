6839f36faee5d9a890e02b8d254e339b51d2bb6a
Tuki-serv <lucasghernandez12@gmail.com>
2024-11-09 19:42:04 -0300

    Corrección

--- Clases.py
+++ Clases.py
@@ -1,11 +1,11 @@
 class Detector:
     BASES_NITROGENADAS = ["A","C","G","T"]
     CANTIDAD_MAXIMA = 3
-    contador = 1
     rango_secuencia = 6
 
     def __init__(self,adn):
         self.adn = adn
+        self.contador = 1
 
 
     def detectar_mutantes(self):
@@ -19,19 +19,17 @@ class Detector:
 
     def horizontal(self):
             for i in range(self.rango_secuencia):
-                print(f"Primer Contador {self.contador}")
+                self.contador = 1
                 self.fila = self.adn[i]
                 for j in range(1, self.rango_secuencia):
                     if self.fila[j] == self.fila[j-1]:
                         self.contador += 1
                     else:
                         self.contador = 1
-                if self.contador > self.CANTIDAD_MAXIMA:
-                    print(f"mutante, {self.contador}")
-                    return True
-                #self.contador = 1
-            print(f"no es mutante,Segundo contador {self.contador}")
+                    if self.contador > self.CANTIDAD_MAXIMA:
+                        return True
             return False
+
     def vertical(self):
             pass
     def diagonal(self):
