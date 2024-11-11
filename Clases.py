0c01210bcb346214a4a7c30e3f7fa794c5ff99e1
Tuki-serv <lucasghernandez12@gmail.com>
2024-11-11 15:02:57 -0300

    Clase-Detector-terminada

--- Clases.py
+++ Clases.py
@@ -1,7 +1,8 @@
 class Detector:
-    CANTIDAD_MAXIMA = 3
-    rango_secuencia = 6
-    coordenadas = {
+    # Valores que son necesarios para detectar mutantes
+    __CANTIDAD_MAXIMA = 3 # Maximo de bases iguales
+    __rango_secuencia = 6 # Rango de las secuencias introducidas
+    __coordenadas = {# Coordendas para iterar en diagonal
     "d1" : [[3,0],[2,1],[1,2],[0,3]],
     "d2" : [[4,0],[3,1],[2,2],[1,3],[0,4]],
     "d3" : [[5,0],[4,1],[3,2],[2,3],[1,4],[0,5]],
@@ -9,66 +10,70 @@ class Detector:
     "d5" : [[5,2],[4,3],[3,4],[2,5]]
     }
 
-
+# Método constructor donde se define la referencia de la matriz
+# y también un contador de bases
     def __init__(self,adn):
         self.adn = adn
         self.contador = 1
 
-
+# Metodo principal que llama al metodo gestor
     def detectar_mutantes(self):
         return print(self.mutantes())
     
-    def mutantes(self):# Aca gestiono los metodos para detectar mutantes
+
+    def mutantes(self):# Metodo que gestiona las distintas direciones
         if self.vertical() == True or self.horizontal()==True or self.diagonal() == True:
-        #if self.diagonal() == True:
             return True
         else:
-            print("Chau")
             return False
-    def verficacion(self, lista,rango):
+        
+    def verficacion(self, lista,rango): #Metodo de verificación
+        # Inicializa lo valores a trabajar
         self.identidad = lista
         self.rango = rango
-        for j in range(1, self.rango):
+        for j in range(1, self.rango):# Bucle que itera sobre la lista
+            # Condición de verificación de lista
+            # Se evalua si se repiten bases más de 3 veces
             if self.identidad[j] == self.identidad[j-1]:
                 self.contador += 1
             else:
                 self.contador = 1
-            if self.contador > self.CANTIDAD_MAXIMA:
+            if self.contador > self.__CANTIDAD_MAXIMA:
                 print(self.identidad)
                 return True
         return False
 
-    def horizontal(self):
-        for i in range(self.rango_secuencia):
+    def horizontal(self):# Dirección horizontal
+        for i in range(self.__rango_secuencia):# Añade los elementos a una lista y la  verifica
             self.contador = 1
             self.fila = self.adn[i]
             fila = self.fila
-            rango = self.rango_secuencia
+            rango = self.__rango_secuencia
             if  self.verficacion(fila,rango) == True:
                 print("Horizontal detectada")
                 return True
         return False
 
-    def vertical(self):
-        for i in range(self.rango_secuencia):
+    def vertical(self):# Dirección vertical
+        for i in range(self.__rango_secuencia):# Añade los elementos a una lista y la  verifica
             self.contador = 1
-            self.columna = [self.adn[j][i] for j in range(self.rango_secuencia)]
+            self.columna = [self.adn[j][i] for j in range(self.__rango_secuencia)]
             fila = self.columna
-            rango = (self.rango_secuencia)
+            rango = (self.__rango_secuencia)
             if self.verficacion(fila,rango):
                 print("Vertical detectada")
                 return True
         return False
 
-    def diagonal(self):
-        self.orden = 2
+    def diagonal(self):# Dirección diagonal
+        self.orden = 2 # Valor de control
         self.contador = 1
-        for i in range(self.orden):
-            if i == 1:
+        for i in range(self.orden):# Segun el valor de control devuelve un lista
+            if i == 1:# La lista puede ser la original o la inversa
                 self.matriz = [list(reversed(sublista)) for sublista in self.adn]
             else:
                 self.matriz = self.adn
-            for d in self.coordenadas.values():
+            for d in self.__coordenadas.values():# Añade los elementos a una lista y la  verifica
                 self.elementos = [self.matriz[j[0]][j[1]] for j in d]
                 fila = self.elementos
                 rango = len(self.elementos)
--- Ejecutable.py
+++ Ejecutable.py
@@ -1,11 +1,3 @@
-"""def mal_input(i,b):
-    while True:
-        secuencia = input(f"Ingrese nuevamente la {i+1}º secuencia de 6 bases nitrogenadas: ")
-        correccion = list(secuencia)
-        if len(correccion) == 6 and all(base in b for base in correccion):
-            break
-    return correccion
-"""
 import Clases
 
 adn = [] 
@@ -15,10 +7,9 @@ TAM_SECUENCIA = 6
 print("Para empezar tendra que incertar una secuencia de ADN")
 print("Si coloca mal alguna secuencia de bases, se le pedira que la ingrese devuelta")
 
-for i in range(TAM_SECUENCIA):
-    while True:
+for i in range(TAM_SECUENCIA):# Bucle de de input de secuencia
+    while True:# Bucle de verificación de input de secuencia
         secuencia = input(f"Ingrese la {i+1}º secuencia de 6 bases nitrogenadas: ").upper()
-
         if len(secuencia) == TAM_SECUENCIA and all(base in BASES_NITROGENADAS for base in secuencia):
             adn.append(list(secuencia))
             break
@@ -26,11 +17,12 @@ for i in range(TAM_SECUENCIA):
             print("Intentelo nuevamente. Secuencia Invalida")
 print("")
 
-"""print("Que es lo que desea hacer?")
+"""
+print("Que es lo que desea hacer?")
 print("Detectar mutantes: D | Mutar ADN: M | Sanar ADN: S")
-option = input("Ingrese D, M o S segun lo que quiera realizar: ").upper()"""
+option = input("Ingrese D, M o S segun lo que quiera realizar: ").upper()
 
-"""if option in ["D", "M", "S"]:
+if option in ["D", "M", "S"]:
     if option == "D":
         adn1 = Clases.Detector(adn)
         adn1.detectar_mutantes()
@@ -45,9 +37,11 @@ option = input("Ingrese D, M o S segun lo que quiera realizar: ").upper()"""
 print("Esta es la secuecia de ADN que usted ingreso")
 for i in range (6):
     for j in range (6):
-        print(f"{adn[i][j]}", end=" ")
+        print(f"{adn[i][j]}", end="  ")
     print("")
-
+print("----------------------------")
+print("")
+print("----------------------------")
 adn1= Clases.Detector(adn)
 adn1.detectar_mutantes()
 input()
